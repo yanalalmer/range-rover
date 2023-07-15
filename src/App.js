d5cbd86b2a821dd9cce1af3e707aa3ece144b805
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2023-07-15 13:14:05 +0200

    added dynamic head titles with react-helmet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { GlobalStyles } from './styles/globalStyles';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <>
+    <HelmetProvider>
+      <Helmet>
+        <meta charset='utf-8' />
+        <link rel='icon' href='favicon.ico' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#000000' />
+        <meta name='description' content='Explore the new Range Rover models' />
+        <title>Range Rover</title>
+      </Helmet>
       <GlobalStyles />
+
       <div className='App'>this is my app now</div>
-    </>
+    </HelmetProvider>
   );
 }
 
